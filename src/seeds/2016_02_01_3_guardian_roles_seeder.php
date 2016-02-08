35e35ad50eeec7ab7d9c16363cd9ddd22cc4e5e1
Cátia Araújo <catiaaraujo@boldint.com>
2016-02-08 10:12:33 +0000

    Seedings role-tokens

--- src/seeds/2016_02_01_3_guardian_roles_seeder.php
+++ src/seeds/2016_02_01_3_guardian_roles_seeder.php
@@ -13,35 +13,72 @@ class GuardianRolesSeeder extends Seeder
     *
     * @return void
     */
-	public function run()
-	{
-		// The Roles
-		$admin = Role::create (
-		[
-			'name'			=>  'Administrator',
-			'description'	=>  'All rights are granted'
-		]);
-		
-		$editor = Role::create (
-		[
-			'name'			=>  'Editor',
-			'description'	=>  'Most rights are granted, except closing'
-		]);
-		
-		$intern = Role::create (
-		[
-			'name'			=>  'Intern',
-			'description'	=>  'Some rights are granted'
-		]);
-		
-		$guest = Role::create (
-		[
-			'name'			=>  'Guest',
-			'description'	=>  'Only limited view rights are granted'
-		]);
-		
-		// The Tokens
-		// prove 4 example tokens, in a way Guest has 1, and admin 4
-		
-	}
+    public function run()
+    {
+        // The Roles
+        $admin = Role::create([
+            'name'          =>  'Administrator',
+            'description'   =>  'All rights are granted'
+        ]);
+
+        $editor = Role::create([
+            'name'          =>  'Editor',
+            'description'   =>  'Most rights are granted, except closing'
+        ]);
+
+        $intern = Role::create([
+            'name'          =>  'Intern',
+            'description'   =>  'Some rights are granted'
+        ]);
+
+        $guest = Role::create([
+            'name'          =>  'Guest',
+            'description'   =>  'Only limited view rights are granted'
+        ]);
+
+        // The Tokens
+        // prove 4 example tokens, in a way Guest has 1, and admin 4
+        $adminView = RoleToken::create([
+            "slug"          =>  "admin:view",
+            "description"   =>  "Admin content"
+        ]);
+
+        $admin = Role::find(1);
+        $admin->roletokens()->save($adminView);
+
+        $noteView = RoleToken::create([
+            "slug"          =>  "note:view",
+            "description"   =>  "Note view content"
+        ]);
+
+        $admin->roletokens()->save($noteView);
+
+        $editor = Role::find(2);
+        $editor->roletokens()->save($noteView);
+
+        $inter = Role::find(3);
+        $inter->roletokens()->save($noteView);
+
+        $guest = Role::find(4);
+        $guest->roletokens()->save($noteView);
+
+
+        $noteEdit = RoleToken::create([
+            "slug"          =>  "note:edit",
+            "description"   =>  "Edit notes view"
+        ]);
+
+        $admin->roletokens()->save($noteEdit);
+        $editor->roletokens()->save($noteEdit);
+        $inter->roletokens()->save($noteEdit);
+
+        $noteCreate = RoleToken::create([
+            "slug"          =>  "note:create",
+            "description"   =>  "Create notes view"
+        ]);
+
+        $admin->roletokens()->save($noteCreate);
+        $editor->roletokens()->save($noteCreate);
+
+    }
 }
